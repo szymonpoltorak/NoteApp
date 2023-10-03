e0e85869e537056d57874423407c849655950c3e
Szymon Półtorak <szymonpotorak@gmail.com>
2023-10-03 12:15:28 +0200

    Init commit

---
+++ note-app-backend/src/test/java/dev/razepl/noteapp/NoteAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.razepl.noteapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
