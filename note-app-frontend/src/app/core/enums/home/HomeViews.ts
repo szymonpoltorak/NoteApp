8006d536141091de671f0291946eec9c2b7da822
Szymon Półtorak <szymonpotorak@gmail.com>
2023-10-21 18:24:22 +0200

    Added switching from hgome to profile view

---
+++ note-app-frontend/src/app/core/enums/home/HomeViews.ts
@@ -0,0 +1,4 @@
+export enum HomeViews {
+    NOTES,
+    PROFILE
+}
--- note-app-frontend/src/app/pages/home/home.component.ts
+++ note-app-frontend/src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Note } from "@core/data/note";
+import { HomeViews } from "@enums/home/HomeViews";
 
 @Component({
     selector: 'app-home',
@@ -8,6 +9,8 @@ import { Note } from "@core/data/note";
 })
 export class HomeComponent implements OnInit {
     readonly notes: Note[] = []
+    protected readonly HomeViews = HomeViews;
+    currentView: HomeViews = HomeViews.NOTES;
 
     ngOnInit(): void {
         for (let i = 0; i < 16; i++) {
@@ -16,6 +19,18 @@ export class HomeComponent implements OnInit {
                 description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed sem nec eros finibus fringilla. Proin sed justo ut elit tincidunt varius. Nullam id urna nec justo vehicula semper. Sed varius risus a purus aliquam, in ultricies odio viverra. Nullam in sapien eu ante viverra vulputate ac eget lectus.",
                 noteId: i + 1
             });
+        }
+    }
+
+    changeToHomeView(): void {
+        if (this.currentView !== HomeViews.NOTES) {
+            this.currentView = HomeViews.NOTES;
+        }
+    }
+
+    changeToProfileView(): void {
+        if (this.currentView !== HomeViews.PROFILE) {
+            this.currentView = HomeViews.PROFILE;
         }
     }
 }
