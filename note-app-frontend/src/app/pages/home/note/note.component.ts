2ffe6692a0acdf67c39965a14d57086b4d80157d
Szymon Półtorak <szymonpotorak@gmail.com>
2023-10-21 12:49:28 +0200

    Added buttons for notes

--- note-app-frontend/src/app/pages/home/note/note.component.ts
+++ note-app-frontend/src/app/pages/home/note/note.component.ts
@@ -6,16 +6,18 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./note.component.scss']
 })
 export class NoteComponent implements OnInit {
+    private readonly MAX_DESCRIPTION_LENGTH: number = 230;
+    private readonly MAX_TITLE_LENGTH: number = 35;
     @Input() noteId: number = 0;
     @Input() title: string = "";
     @Input() description: string = "";
 
     ngOnInit(): void {
-        if (this.description.length > 250) {
-            this.description = `${this.description.substring(0, 250)}...`
+        if (this.description.length > this.MAX_DESCRIPTION_LENGTH) {
+            this.description = `${this.description.substring(0, this.MAX_DESCRIPTION_LENGTH)}...`
         }
-        if (this.title.length > 35) {
-            this.title = `${this.title.substring(0, 35)}...`;
+        if (this.title.length > this.MAX_TITLE_LENGTH) {
+            this.title = `${this.title.substring(0, this.MAX_TITLE_LENGTH)}...`;
         }
     }
 }
