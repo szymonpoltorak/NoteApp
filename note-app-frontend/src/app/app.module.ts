1aeb2a9d0ffa088e11dc8f86c0f21e46293774c9
Szymon Półtorak <szymonpotorak@gmail.com>
2023-11-26 18:48:56 +0100

    Added reponsiveness to login page

--- note-app-frontend/src/app/app.module.ts
+++ note-app-frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
 import { AuthInterceptor } from "@core/interceptors/auth.interceptor";
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from "@environments/environment";
 
 @NgModule({
     declarations: [
@@ -18,10 +19,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
         BrowserAnimationsModule,
         HttpClientModule,
         ServiceWorkerModule.register('ngsw-worker.js', {
-          enabled: !isDevMode(),
-          // Register the ServiceWorker as soon as the application is stable
-          // or after 30 seconds (whichever comes first).
-          registrationStrategy: 'registerWhenStable:30000'
+            enabled: environment.production,
         })
     ],
     providers: [
