1360ffb5c24f3c397623d9befea35710229fc411
Szymon Półtorak <szymonpotorak@gmail.com>
2023-11-29 18:23:32 +0100

    Added updateVersion script for actions

---
+++ note-app-backend/src/main/resources/updateVersion.py
@@ -0,0 +1,31 @@
+import re
+
+
+def main() -> None:
+    regex: str = r"version = '(\d+).(\d+).(\d+)\S+'"
+    path: str = "../../../build.gradle"
+
+    with open(path, "r") as build:
+        content: str = build.read()
+    matched = re.search(pattern=regex, string=content)
+    match: str = matched.group()
+
+    main_release: int = int(matched.group(1))
+    update: int = int(matched.group(2))
+
+    if update == 20:
+        update = 0
+        patch = 0
+        main_release += 1
+    else:
+        update += 1
+        patch = 0
+
+    with open(path, "w") as build:
+        new_version: str = f"version = '{main_release}.{update}.{patch}'"
+
+        build.write(content.replace(match, new_version))
+
+
+if __name__ == '__main__':
+    main()
